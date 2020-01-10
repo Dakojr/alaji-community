721d162dbd84932a553c94d7ef67914a26544b23
djoun52 <simon.rioux.trv@gmail.com>
2020-01-10 15:29:34 +0100

    remise égale avec michal

--- Controler/ControlerSQL.php
+++ Controler/ControlerSQL.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once('../Modele/connect.php');
 
 
 
@@ -13,32 +14,28 @@ session_start();
 
 function all_users() //cherche tout les données de la table users
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT `id_user`, `nom`, `prenom`, `role`, `date_de_naissance`, `pays_de_naissance`, `sexe`, `adresse`, `ville`, `code_postal`, `telephone`, `email`, `img_path` FROM `users` ');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT `id_user`, `nom`, `prenom`, `role`, `date_de_naissance`, `pays_de_naissance`, `sexe`, `adresse`, `ville`, `code_postal`, `telephone`, `email`, `img_path` FROM `users` ');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
 }
-function users_by_ID() //cherche les données d'un utilisateur par son ID
+function user_by_ID() //cherche les données d'un utilisateur par son ID
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT `id_user`, `nom`, `prenom`, `role`, `date_de_naissance`, `pays_de_naissance`, `sexe`, `adresse`, `ville`, `code_postal`, `telephone`, `email`, `img_path` FROM `users` WHERE ID_user = "1"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT `id_user`, `nom`, `prenom`, `role`, `date_de_naissance`, `pays_de_naissance`, `sexe`, `adresse`, `ville`, `code_postal`, `telephone`, `email`, `img_path` FROM `users` WHERE ID_user = "1"');
     $stmt->execute();
-    $result = $stmt->fetchall();
+    $result = $stmt->fetch();
     return $result;
 }
 function users_by_formation() //cherche les données d'un utilisateur par sa formation
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT `id_user`, `nom`, `prenom`, `role`, `date_de_naissance`, `pays_de_naissance`, `sexe`, `adresse`, `ville`, `code_postal`, `telephone`, `email`, `img_path` FROM `users` WHERE ID = "1"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT `id_user`, `nom`, `prenom`, `role`, `date_de_naissance`, `pays_de_naissance`, `sexe`, `adresse`, `ville`, `code_postal`, `telephone`, `email`, `img_path` FROM `users` WHERE ID = "1"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
 }
 function all_categorie() //cherche tout les données de la table cétégorie
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT * FROM categorie');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM categorie');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
@@ -48,8 +45,7 @@ function all_categorie() //cherche tout les données de la table cétégorie
 
 function all_formation() //cherche tout les données de la table formation
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT * FROM formation ');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM formation ');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
@@ -57,8 +53,7 @@ function all_formation() //cherche tout les données de la table formation
 
 function all_admin() //cherche les données de tout les administrateur
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT * FROM users WHERE role = "admin"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM users WHERE role = "admin"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
@@ -66,8 +61,7 @@ function all_admin() //cherche les données de tout les administrateur
 
 function all_formateur() //cherche les données de tout les formateurs
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT * FROM users WHERE role = "formateur"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM users WHERE role = "formateur"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
@@ -75,8 +69,7 @@ function all_formateur() //cherche les données de tout les formateurs
 
 function all_eleves() //
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT * FROM users WHERE role = "eleve"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM users WHERE role = "eleve"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
@@ -86,7 +79,7 @@ function all_eleves() //
 function ID_and_nom_formateur() //cherche les ID et les nom de tout les formateurs
 {
     require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT `id_user`, `nom` FROM `users` WHERE role = "formateur"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT `id_user`, `nom` FROM "users" WHERE role = "formateur"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;}
@@ -94,7 +87,7 @@ function ID_and_nom_formateur() //cherche les ID et les nom de tout les formateu
 function IDcategorie_and_nomformation() //cherche les ID des catégories de formation et les noms des formations
 {
     require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT `nom_formation`,`	id_categorie` FROM `formation`');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT `nom_formation`,`id_categorie` FROM "formation"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
@@ -103,7 +96,7 @@ function IDcategorie_and_nomformation() //cherche les ID des catégories de form
 function nomformation_and_IDformation_and_IDcatégorie() //cherche les noms des formations, l'ID des formations et les ID des catégories de formation 
 {
     require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT `	id_formation`,`nom_formation`,`id_categorie` FROM `formation`');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT `id_formation`,`nom_formation`,`id_categorie` FROM "formation"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
@@ -111,12 +104,21 @@ function nomformation_and_IDformation_and_IDcatégorie() //cherche les noms des 
 
 function ID_and_nom_of_formateur() //cherche les ID et les noms de tout les formateurs
 {
-    require_once('../Modele/connect.php');
-    $stmt = $bdd->prepare('SELECT 	"id_user","nom"  FROM users WHERE role = "formateur"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT `id_user`, `nom`  FROM users WHERE role = "formateur"');
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
 }
+// function formation_by_categorie()
+// {
+//     $stmt = $GLOBALS['bdd']->prepare('SELECT "id_user","nom"  FROM users WHERE categorie = "1"');
+//     $stmt->execute();
+//     $result = $stmt->fetch();
+//     return $result;
+// }
 
 $a = all_categorie();
 var_dump($a);
+
+$b = all_admin();
+var_dump($b);
--- Modele/connect.php
+++ Modele/connect.php
@@ -5,10 +5,10 @@ $dbuser = "root";
 $dbpass = "";
 
 try {
-    $bdd = new PDO($dsn, $dbuser, $dbpass);
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $bdd->exec("SET CHARACTER SET utf8");
+    $GLOBALS['bdd'] = new PDO($dsn, $dbuser, $dbpass);
+    $GLOBALS['bdd']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $GLOBALS['bdd']->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $GLOBALS['bdd']->exec("SET CHARACTER SET utf8");
 } catch (PDOException $err) {
     $now = new DateTime("", new DateTimeZone('Europe/Paris'));
     $now = $now->format("d-M-Y H:i:s");
