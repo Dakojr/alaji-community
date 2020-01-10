b7aa3334c63d52da6d97a29642c885ae4dec3f49
Moxtak <probarbillonm@gmail.com>
2020-01-10 13:36:17 +0100

    debug controller authentification

--- Vue/inscription.php
+++ Vue/inscription.php
@@ -28,15 +28,7 @@ Nom : <input type="text" name="nom" size="12"><br>
 Prénom : <input type="text" name="prenom" size="12">
 date de naissance : <input type="date" name="date de naissance" size="12">
 pays de naissance : <input type="text" name="pays de naissance" size="12"><br>
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Sexe
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">homme</a>
-    <a class="dropdown-item" href="#">femme</a>
-  </div>
-</div>
+sexe : <input type="text" name="sexe" size="12"><br>
 adresse : <input type="text" name="adresse" size="12"><br>
 ville : <input type="text" name="ville" size="12">
 code postal : <input type="text" name="codde postal" size="12">
@@ -52,7 +44,7 @@ image de profil : <input type="text" name="image de profil" size="12"><br>
 
     <!-- JQuery -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- Bootstrap tooltips -->
+    <!- - Bootstrap tooltips -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
     <!-- Bootstrap core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
