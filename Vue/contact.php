8b2ea46c57a9595e891fcf2762e0309a878eca72
Choysi <Junco.valentin@gmail.com>
2020-01-10 15:29:26 +0100

    Contact+début_formations

--- Vue/contact.php
+++ Vue/contact.php
@@ -11,15 +11,261 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/css/mdb.min.css" rel="stylesheet">
+    <!-- Link contact CSS -->
     <title>Community Alaji</title>
+    <style>
+        .map-container-6 {
+            overflow: hidden;
+            padding-bottom: 56.25%;
+            position: relative;
+            height: 0;
+        }
+
+        .map-container-6 iframe {
+            left: 0;
+            top: 0;
+            height: 100%;
+            width: 100%;
+            position: absolute;
+        }
+
+        #Ecris_nous {
+            padding-left: 3px;
+        }
+    </style>
 </head>
 
 <body>
+    <nav class="mb-1 navbar navbar-expand-lg navbar-dark border-bottom" style="background-color: #FDD835">
+        <a class="navbar-brand" href="index.php"><img style="background-color: white; border-radius:200px;padding:5px;width:70px;" src="https://nsa40.casimages.com/img/2020/01/10/200110014602150184.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle btn-link rounded-pill" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formations</a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                        <a class="dropdown-item waves-light" href="#">Toutes les formations</a>
+                        <a class="dropdown-item waves-light" href="#">Formations par catégories</a>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto nav-flex-icons">
+                <li class="nav-item">
+                    <a class="nav-link waves-light btn-link rounded-circle" href="inscription.php">Inscription</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link waves-light btn-link rounded-circle" href="connexion.php">Connexion</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!--/.Navbar -->
+    <div class="text-center">
+        <button type="button" class="d-inline-block btn btn-#fdd835 yellow darken-1 animated fadeIn " style="width: 300px;height:120px;">Contacte-nous</button>
+    </div>
+    <!-- -----------------------------------------------    -->
+    <!-- administration -->
+    <div>
+        <!--Section: Testimonials v.1-->
+        <section class="section pb-3 text-center">
 
+            <!--Section heading-->
+            <h1 class="section-heading h1 pt-4">L'administration</h1>
+            <!--Section description-->
+            <p class="section-description">Une formation digne des plus grande !</p>
 
+            <div class="row">
 
-    <h1>Hello Home !</h1>
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-12 mb-4">
 
+                    <!--Card-->
+                    <div class="card testimonial-card animated fadeIn low delay-50ms">
+
+                        <!--Background color-->
+                        <div class="card-up teal lighten-2">
+                        </div>
+
+                        <!--Avatar-->
+                        <div class="avatar mx-auto white"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" alt="avatar mx-auto white" class="rounded-circle img-fluid">
+                        </div>
+
+                        <div class="card-body">
+                            <!--Name-->
+                            <h4 class="card-title mt-1">Hémione bourricot</h4>
+                            <hr>
+                            <!--Quotation-->
+                            <p><i class="fas fa-quote-left"></i> Depuis que j'ai crée mon compte, ma vie à changé !</p>
+                        </div>
+
+                    </div>
+                    <!--Card-->
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-12 mb-4">
+
+                    <!--Card-->
+                    <div class="card testimonial-card animated fadeIn low delay-50ms">
+
+                        <!--Background color-->
+                        <div class="card-up blue lighten-2">
+                        </div>
+
+                        <!--Avatar-->
+                        <div class="avatar mx-auto white"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" alt="avatar mx-auto white" class="rounded-circle img-fluid">
+                        </div>
+
+                        <div class="card-body">
+                            <!--Name-->
+                            <h4 class="card-title mt-1">Anne Bourricot</h4>
+                            <hr>
+                            <!--Quotation-->
+                            <p><i class="fas fa-quote-left"></i> Je suis très à cheval sur le règlement !</p>
+                        </div>
+
+                    </div>
+                    <!--Card-->
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-4 col-md-12 mb-4">
+
+                    <!--Card-->
+                    <div class="card testimonial-card animated fadeIn low delay-50ms">
+
+                        <!--Background color-->
+                        <div class="card-up deep-purple lighten-2"></div>
+
+                        <!--Avatar-->
+                        <div class="avatar mx-auto white "><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" alt="avatar mx-auto white" class="rounded-circle img-fluid">
+                        </div>
+
+                        <div class="card-body">
+                            <!--Name-->
+                            <h4 class="card-title mt-1">Marie Boulet</h4>
+                            <hr>
+                            <!--Quotation-->
+                            <p><i class="fas fa-quote-left"></i> Pierre qui roule n'amasse pas mousse !</p>
+                        </div>
+
+                    </div>
+                    <!--Card-->
+
+                </div>
+                <!--Grid column-->
+
+            </div>
+
+        </section>
+        <!--Section: Testimonials v.1-->
+        <!-- ------------------------------------------- -->
+        <!--Section: Contact v.1-->
+        <section class="section pb-5">
+
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-lg-5 mb-4">
+
+                    <!--Form with header-->
+                    <div class="card">
+
+                        <div class="card-body">
+                            <!--Header-->
+                            <div id="Ecris_nous" class="form-header #fdd835 yellow darken-1">
+                                <h3><i class="fas fa-envelope"></i> Ecris nous :</h3>
+                            </div>
+
+                            <p>Nous te répondrons rapidement !</p>
+                            <br>
+
+                            <!--Body-->
+                            <div class="md-form">
+                                <i class="fas fa-user prefix grey-text"></i>
+                                <input type="text" id="form-name" class="form-control">
+                                <label for="form-name">Ton nom</label>
+                            </div>
+
+                            <div class="md-form">
+                                <i class="fas fa-envelope prefix grey-text"></i>
+                                <input type="text" id="form-email" class="form-control">
+                                <label for="form-email">ton email</label>
+                            </div>
+
+                            <div class="md-form">
+                                <i class="fas fa-tag prefix grey-text"></i>
+                                <input type="text" id="form-Subject" class="form-control">
+                                <label for="form-Subject">Sujet</label>
+                            </div>
+
+                            <div class="md-form">
+                                <i class="fas fa-pencil-alt prefix grey-text"></i>
+                                <textarea id="form-text" class="form-control md-textarea" rows="3"></textarea>
+                                <label for="form-text">Ton message</label>
+                            </div>
+
+                            <div class="text-center mt-4">
+                                <button class="btn btn-#fdd835 yellow darken-1">Envoyer</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!--Form with header-->
+
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-7">
+
+                    <!--Google map-->
+                    <div id="map-container-google-11" class="z-depth-1-half map-container-6" style="height: 400px">
+                        <iframe src="https://maps.google.com/maps?q=new%20delphi&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+
+                    <br>
+                    <!--Buttons-->
+                    <div class="row text-center">
+                        <div class="col-md-4">
+                            <a style="color:black;" class="btn #fdd835 yellow darken-1"><i class="fas fa-map-marker-alt"></i></a>
+                            <p>7 Rue de la Haye</p>
+                            <p>Immeuble l’Atrya</p>
+                            <p>67300 Schiltigheim</p>
+                        </div>
+
+                        <div class="col-md-4">
+                            <a style="color:black;" class="btn #fdd835 yellow darken-1"><i class="fas fa-phone"></i></a>
+                            <p>03 88 35 07 79</p>
+                            <p>Mon - Fri, 8:00-22:00</p>
+                        </div>
+
+                        <div class="col-md-4">
+                            <a style="color:black;" class="btn #fdd835 yellow darken-1"><i class="fas fa-envelope"></i></a>
+                            <p>info@gmail.com</p>
+                            <p>sale@gmail.com</p>
+                        </div>
+                    </div>
+
+                </div>
+                <!--Grid column-->
+
+            </div>
+
+        </section>
+        <!--Section: Contact v.1-->
+    </div>
     <!-- JQuery -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- Bootstrap tooltips -->
--- Vue/formation.php
+++ Vue/formation.php
@@ -11,23 +11,90 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/css/mdb.min.css" rel="stylesheet">
+    <!-- Link contact CSS -->
     <title>Community Alaji</title>
+    <style>
+        body,
+        html {
+            height: 100%;
+        }
+
+        .bg {
+            /* The image used */
+            background-image: url("../assets/img/Background_Formations.jpg");
+            background-color: rgba(117, 190, 218, 0.5);
+            /* Full height */
+            height: 100%;
+
+            /* Center and scale the image nicely */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
 </head>
+<!--/.Navbar -->
 
-<body>
+<body class="bg">
 
+    <nav class="mb-1 navbar navbar-expand-lg navbar-dark border-bottom" style="background-color: #FDD835">
+        <a class="navbar-brand" href="index.php"><img style="background-color: white; border-radius:200px;padding:5px;width:70px;" src="https://nsa40.casimages.com/img/2020/01/10/200110014602150184.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333" aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle btn-link rounded-pill" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Formations</a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                        <a class="dropdown-item waves-light" href="#">Toutes les formations</a>
+                        <a class="dropdown-item waves-light" href="#">Formations par catégories</a>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto nav-flex-icons">
+                <li class="nav-item">
+                    <a class="nav-link waves-light btn-link rounded-circle" href="inscription.php">Inscription</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link waves-light btn-link rounded-circle" href="connexion.php">Connexion</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!--/.Navbar -->
+    <div class="text-center">
+        <button type="button" class="d-inline-block btn btn-#fdd835 yellow darken-1 animated fadeIn " style="width: 300px;height:120px;">Formations</button>
+    </div>
+    <!-- -----------------------------------------------    -->
+    <!-- administration -->
+    <div>
+        <!--Section: Testimonials v.1-->
+        <section class="section pb-3 text-center">
 
+            <!--Section heading-->
+            <h1 style="color:white;" class="section-heading h1 pt-4">Les Formations</h1>
+            <!--Section description-->
+            <p style="color:white;" class="section-description">De sage femme à développeur WEB</p>
+            <!--Bakground_Image -->
 
-    <h1>Hello Home !</h1>
+            <p style="color:white;" class="py-3 text-center">Voici les différentes formations qu'Alaji propose...</p>
+            <!--/.Bakground_Image -->
+</body>
 
-    <!-- JQuery -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/js/mdb.min.js"></script>
+</div>
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/js/mdb.min.js"></script>
 </body>
 
 </html>
