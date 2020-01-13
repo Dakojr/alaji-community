dd5e0cff66eb0c7663f0914073b4c627cd2e9561
djoun52 <simon.rioux.trv@gmail.com>
2020-01-13 16:41:22 +0100

    fin de journée

--- Controler/ControlerSQL.php
+++ Controler/ControlerSQL.php
@@ -18,9 +18,24 @@ function all_users() //cherche tout les données de la table users
 
 function all_categorie() //cherche tout les données de la table cétégorie
 {
-    $stmt = $GLOBALS['bdd']->prepare('SELECT `id_user`, `nom`, `prenom`, `role`, `date_de_naissance`, `pays_de_naissance`, `sexe`, `adresse`, `ville`, `code_postal`, `telephone`, `email`, `img_path` FROM `users` WHERE ID_user = "1"');
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM `categorie`');
     $stmt->execute();
     $result = $stmt->fetch();
+    return $result;
+}
+
+function all_formation() //cherche tout les données de la table formation
+{
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM formation ');
+    $stmt->execute();
+    $result = $stmt->fetchall();
+    return $result;
+}
+function all_eleve_formation() // cherche tout les données de la table eleve_formation
+{
+    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM eleve_formation ');
+    $stmt->execute();
+    $result = $stmt->fetchall();
     return $result;
 }
 
@@ -31,20 +46,8 @@ function users_by_ID() //cherche les données d'un utilisateur par son ID
     $result = $stmt->fetchall();
     return $result;
 }
-// function users_by_formation() //cherche les données d'un utilisateur par sa formation
-// {
-//     $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM categorie');
-//     $stmt->execute();
-//     $result = $stmt->fetchall();
-//     return $result;
-// }
-function all_formation() //cherche tout les données de la table formation
-{
-    $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM formation ');
-    $stmt->execute();
-    $result = $stmt->fetchall();
-    return $result;
-}
+
+
 function all_admin() //cherche les données de tout les administrateur
 {
     $stmt = $GLOBALS['bdd']->prepare('SELECT * FROM users WHERE role = "admin"');
@@ -94,28 +97,89 @@ function ID_and_nom_of_formateur() //cherche les ID et les noms de tout les form
     $result = $stmt->fetchall();
     return $result;
 }
-function users_by_formation() // 
+function users_by_formation() // cherche les informations des eleves et des formateurs d'une formation 
 {
-    $stmt = $GLOBALS['bdd']->prepare("SELECT u.id_user, u.nom, u.prenom, u.role, u.date_de_naissance, u.pays_de_naissance, u.sexe, u.adresse, u.ville, u.code_postal, u.telephone, u.email, u.img_path, f.id_formation, f.nom_formation, f.lieu, f.nb_eleve_max, f.lien_foarmation_slack, f.date_de__debut, f.date_de_fin, ef.id_formation, ef.id_user FROM users as u, formation as f, eleve_formation as ef  WHERE u.id_user=ef.id_user AND f.id_formation = ef.id_formation and f.id_formation = '1'");
+    $stmt = $GLOBALS['bdd']->prepare("SELECT u.id_user, u.nom, u.prenom, u.role, u.date_de_naissance, u.pays_de_naissance, u.sexe, u.adresse, u.ville, u.code_postal, u.telephone, u.email, u.img_path, f.id_formation, f.nom_formation, f.lieu, f.nb_eleve_max, f.lien_foarmation_slack, f.date_de_debut, f.date_de_fin, ef.id_formation, ef.id_user FROM users as u, formation as f, eleve_formation as ef  WHERE u.id_user=ef.id_user AND f.id_formation = ef.id_formation and f.id_formation = '1'");
     $stmt->execute();
     $result = $stmt->fetchall();
     return $result;
 }
 
-function formation_by_categorie()
+function formation_by_categorie() // cherche tout les information de tout les formation d'une catégorie de formation
 {
-    $stmt = $GLOBALS['bdd']->prepare("SELECT f.id_formation, f.nom_formation, f.lieu, f.nb_eleve_max,f.lien_foarmation_slack, f.date_de__debut, f.date_de_fin, f.id_user, c.id_categorie, c.categorie FROM formation as f, categorie as c WHERE f.id_categorie = '1' and f.id_categorie= c.id_categorie");
+    $stmt = $GLOBALS['bdd']->prepare("SELECT f.id_formation, f.nom_formation, f.lieu, f.nb_eleve_max,f.lien_foarmation_slack, f.date_de_debut, f.date_de_fin, f.id_user, c.id_categorie, c.categorie FROM formation as f, categorie as c WHERE f.id_categorie = '1' and f.id_categorie= c.id_categorie");
     $stmt->execute();
-    $result = $stmt->fetch();
+    $result = $stmt->fetchall();
     return $result;
 }
 
-function formateur_by_formation()
+function formateur_by_formation() // cherche tout les information des formateur d'une formation 
 {
     $stmt = $GLOBALS['bdd']->prepare("SELECT u.id_user, u.nom, u.prenom, u.role, u.date_de_naissance, u.pays_de_naissance, u.sexe, u.adresse, u.ville, u.code_postal, u.telephone, u.email, u.img_path, f.nom_formation,f.nb_eleve_max FROM users as u, formation as f WHERE role = 'formateur' AND u.id_user = f.id_user AND f.id_formation = '2'");
     $stmt->execute();
-    $result = $stmt->fetch();
+    $result = $stmt->fetchall();
     return $result;
 }
-$a = formateur_by_formation();
-var_dump($a);
+
+function eleve_by_formation() // cherche tout les information des eleves d'une formation
+{
+    $stmt = $GLOBALS['bdd']->prepare("SELECT u.id_user, u.nom, u.prenom, u.role, u.date_de_naissance, u.pays_de_naissance, u.sexe, u.adresse, u.ville, u.code_postal, u.telephone, u.email, u.img_path, f.nom_formation,f.nb_eleve_max FROM users as u, formation as f, eleve_formation as ef WHERE u.role ='eleve' AND u.id_user = ef.id_user AND ef.id_formation = '1'");
+    $stmt->execute();
+    $result = $stmt->fetchall();
+    return $result;
+}
+
+function nb_max_of_eleve_by_formation() // nombre maximum eleve d'une formation
+{
+    $stmt = $GLOBALS['bdd']->prepare("SELECT `nb_eleve_max` FROM formation WHERE id_formation = '2'");
+    $stmt->execute();
+    $nmoebf = $stmt->fetch();
+    $result = 0;
+    foreach ($nmoebf as $key => $value) {
+        $result=$value;
+    }
+    return $result;
+}
+function nb_eleve_by_formation() // nbr eleve inscrit sur une formation
+{
+    $ebf= eleve_by_formation();
+    $result = 0;
+    foreach ($ebf as $key => $value) {
+        $result++;
+    }
+    return $result;
+}
+
+function nb_place_restent_by_formation() // nombre de place restent dans une formation
+{
+    $nmoebf = nb_max_of_eleve_by_formation();
+    $nebf = nb_eleve_by_formation();
+    $result = $nmoebf-$nebf;
+    return $result;   
+
+}
+
+function trois_prochaine_formation() // cherche le noms des trois prochaine formation
+{
+    $stmt = $GLOBALS['bdd']->prepare("SELECT `date_de_debut` FROM formation");
+    $stmt->execute();
+    $nmoebf = $stmt->fetch();
+
+    $stmt = $GLOBALS['bdd']->prepare("SELECT CURDATE()");
+    $stmt->execute();
+    $date = $stmt->fetch();
+    $s1= 0;
+    $s= [];
+    foreach ($date as $key => $value) {
+        $s1 = $value ;
+    }
+    foreach ($nmoebf as $key => $value) {
+        if ($value > $s1){
+            array_push($s,$value);
+        }
+    }
+}
+
+$c = trois_prochaine_formation();
+echo $c;
+var_dump($c);
